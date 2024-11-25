d13ef43a9094bb39be581df39432fd45ab84fb32
alvinjwong <ajwong@uab.edu>
2024-11-25 22:09:45 +0000

    finish .h

--- vigenere.h
+++ vigenere.h
@@ -2,3 +2,27 @@
 #define VIGENERE_H
 
 #include <string>
+
+
+class Vigenere {
+    public:
+    Vigenere(std::string key);
+
+    ~Vigenere();
+
+    std::string encrypt(std::string msg);
+
+    std::string decrypt(std::string msg);
+
+    bool isEncrypted(std::string encryptedMsg, std::string decryptedMessage);
+
+    void setKey(std::string key);
+
+    private:
+    std::string key_t;
+    
+    std::string repeatKey(std::string msg);
+
+};
+
+#endif
