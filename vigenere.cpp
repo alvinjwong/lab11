6ce23837d13960561f3d4d435f8fc2ae10e70484
alvinjwong <ajwong@uab.edu>
2024-11-25 22:39:46 +0000

    written some mroe code

--- vigenere.cpp
+++ vigenere.cpp
@@ -1,1 +1,20 @@
 #include "vigenere.h"
+#include <cctype>
+#include <algorithm>
+
+Vigenere::Vigenere(std::string key) : key(std::move(key)) {}
+
+Vigenere::~Vigenere() {}
+
+std::string Vigenere::repeatKey(std::string msg) {
+    std::string repeatedKey;
+    int keyIndex = 0;
+    for (char c : msg) {
+        if (std::isalpha(c)) {
+            repeatedKey += key[keyIndex];
+            keyIndex = (keyIndex + 1) % key.size();
+        }
+    }
+    return repeatedKey;
+}
+
