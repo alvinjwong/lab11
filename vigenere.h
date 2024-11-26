9b98557f895dc8d8ed3631c7f8db36b53df54667
alvinjwong <ajwong@uab.edu>
2024-11-26 05:03:20 +0000

    added comments for doxy\

--- vigenere.h
+++ vigenere.h
@@ -6,22 +6,59 @@
 
 class Vigenere {
     public:
+    /**
+    * @brief constructs vigenere object with a key
+    * @param key
+    */
     Vigenere(std::string key);
 
+/** 
+*@brief destructor for the vigenere
+*/
     ~Vigenere();
 
+/**
+ * @brief encrypts a text message using the current key
+ * each letter is shifted forwards in the alphabet based on the corresponding letter in the key
+ * @param msg
+ * @return encrypted message
+ */
     std::string encrypt(std::string msg);
 
+/**
+ * @brief decrypts the encrypted message using the key
+ * each letter is shifted backwards in the alphabet based on the corresponding letter in the key
+ * @param msg
+ * @return decrypted message
+ */
     std::string decrypt(std::string msg);
 
+/**
+ * @brief checks to see if encrypted message matches the decrypted message
+ * 
+ * @param encryptedMsg
+ * @param decryptedMessage
+ * @return True if decrypted message matches original message, false if not
+ */
     bool isEncrypted(std::string encryptedMsg, std::string decryptedMessage);
 
+/**
+ * @brief sets a new key
+ * @param key
+ */
     void setKey(std::string key);
 
     private:
     std::string key;
+    /**
+     * @brief key used for encryption/decryption
+     */
     
     std::string repeatKey(std::string msg);
+    /**
+     * @brief repeats key to match length of message
+     * @param msg
+     */
 
 };
 
